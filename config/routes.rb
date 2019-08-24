b29a596ceb46fddfc40eea9be8ecee5f7d31ff3f
shinohara <shinohello0420@yahoo.co.jp>
2019-08-24 20:55:58 +0900

    o

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'messages#index'
-
+  devise_for :users
+  root 'groups#index'
+  resource :user, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
