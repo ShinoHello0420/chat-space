d1e249cb3d25c44655d5f921a5792a4fbba7ad14
shinohara <shinohello0420@yahoo.co.jp>
2019-09-05 21:27:33 +0900

    rw

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,17 +1,16 @@
 $(document).on('turbolinks:load', function() {  
 //$(function() {
+  var search_list = $("#user-search-result");
   function appendUser(user) {
-
-    var search_list = $("#user-search-result");
-
-
-    // function appendList(user) {
+  
+  // function appendList(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
     search_list.append(html);
     // }
+    return html;
 
   }
     function appendMembers(name, user_id) {
@@ -25,6 +24,7 @@ $(document).on('turbolinks:load', function() {
      member_list.append(html);
     }
 
+  $(function(){ 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
   
@@ -49,10 +49,10 @@ $(document).on('turbolinks:load', function() {
         alert('ユーザー検索に失敗しました');
       })
     });
-
+  });
     
     
-    // $(function(){
+    $(function(){
       $(document).on('click', '.user-search-add', function() {
         var user_id = $(this).data("user-id");
         var name = $(this).data("user-name");
@@ -63,8 +63,8 @@ $(document).on('turbolinks:load', function() {
       $(document).on("click", '.user-search-remove', function() {
         $(this).parent().remove();
       });
-    // });
-
+     });
+ 
   });
   
 
